e59bf42ef86d4237db4034e8c74a0bda555ae1de
andros <andros2011@gmail.com>
2013-04-26 21:46:17 -0400

    No longer needed

--- full_infuse4g.mk
+++ full_infuse4g.mk
@@ -20,7 +20,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/infuse4g/infuse4g.mk)
 
 # Inherit from those products. Most specific first.
