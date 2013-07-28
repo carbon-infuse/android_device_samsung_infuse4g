f588bb3bf07f7ae5aeb06311510c105d7bcf1faf
Andros Rosa <andros2011@gmail.com>
2013-07-28 12:56:13 -0400

    Recarbonize
    Change-Id: Idb639ff9f7d62f31b60c435c9b015a301cd12570
    Signed-off-by: Andros Rosa <andros2011@gmail.com>

--- carbon.mk
+++ carbon.mk
@@ -1,23 +1,21 @@
-# Infuse is a GSM phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := Infuse4G
 
-TARGET_BOOTANIMATION_NAME := 480
+# Infuse is a GSM phone
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/infuse4g/full_infuse4g.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := infuse4g
-PRODUCT_NAME := cm_infuse4g
+PRODUCT_NAME := carbon_infuse4g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-I997
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.infuse4g.$(shell date +%m%d%y).$(shell date +%H%M)
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I997 TARGET_DEVICE=SGH-I997 BUILD_FINGERPRINT=samsung/SGH-I997/SGH-I997:2.3.6/GINGERBREAD/UCLB3:user/release-keys PRIVATE_BUILD_DESC="SGH-I997-user 2.3.6 GINGERBREAD UCLB3 release-keys"
-
