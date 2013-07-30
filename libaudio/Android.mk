29426220a8cf29711b978f22894d64a333885436
Andros Rosa <andros2011@gmail.com>
2013-07-30 17:12:25 -0400

    4.3 bringup
    Change-Id: I88e28005f004e919b7573cf8857e7e84d2059b56
    Signed-off-by: Andros Rosa <andros2011@gmail.com>

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -4,25 +4,26 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= \
-	AudioHardware.cpp
+    AudioHardware.cpp
 
 LOCAL_MODULE := audio.primary.aries
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_STATIC_LIBRARIES:= libmedia_helper
 LOCAL_SHARED_LIBRARIES:= \
-	libutils \
-	libhardware_legacy \
-	libtinyalsa \
-	libaudioutils
+    liblog \
+    libutils \
+    libhardware_legacy \
+    libtinyalsa \
+    libaudioutils
 
 LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SHARED_LIBRARIES += libdl
 LOCAL_C_INCLUDES += \
-	external/tinyalsa/include \
-	$(call include-path-for, audio-effects) \
-	$(call include-path-for, audio-utils)
+    external/tinyalsa/include \
+    $(call include-path-for, audio-effects) \
+    $(call include-path-for, audio-utils)
 
 include $(BUILD_SHARED_LIBRARY)
 
